3d8db810977201005bb66a428e7475c9ffa8c89c
j-cleary914 <j.cleary914@gmail.com>
2020-04-11 19:49:50 +0100

    set up file structure and delete some unnecesary files

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}> Travel Widget</h1>
+    </div>
+  );
+};
+
+export default Header;
