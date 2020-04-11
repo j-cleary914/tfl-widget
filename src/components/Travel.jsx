b4a3c84a031868c5737105a994c4f60744f95a2f
j-cleary914 <j.cleary914@gmail.com>
2020-04-11 20:35:14 +0100

    add fetchData function

--- src/components/Travel.jsx
+++ src/components/Travel.jsx
@@ -1,6 +1,41 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Travel extends Component {
+  state = {
+    tubeLineStatuses: [],
+    lastUpdatedString: ""
+  };
+
+  fetchData() {
+    axios
+      .get(
+        "https://api.tfl.gov.uk/Line/Mode/tube/Status?app_id=cdf2be4d&app_key=2c620fbd67b0fe7202e99b15a97419d6"
+      )
+      .then(response => {
+        const tubeLineStatuses = response.data.map(tubeLine => {
+          return {
+            id: tubeLine.id,
+            tubeLineName: tubeLine.name,
+            tubeLineStatus: tubeLine.lineStatuses[0].statusSeverityDescription,
+            tubeLineStatusMessage: tubeLine.lineStatuses[0].reason
+          };
+        });
+
+        this.setState({ tubeLineStatuses });
+      });
+  }
+
+  fetchDate() {
+    const now = new Date();
+    this.setState({ lastUpdatedString: now });
+  }
+
+  componentDidMount() {
+    this.fetchData();
+    this.fetchDate();
+  }
+
   render() {
     return (
       <div>
